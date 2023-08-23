84f99abd6df2a6d96f196b8e09eb348e658c280d
Kartikeya <pandeykartikey012@gmail.com>
2023-08-23 20:56:43 +0530

    made home page

--- src/app.jsx
+++ src/app.jsx
@@ -2,32 +2,22 @@ import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
 import './app.css'
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './pages/home/Home'
+import Dashboard from './pages/dashboard/Dashboard'
 export function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
 
+    },
+    {
+      path:"/dashboard",
+      element:<Dashboard/>,
+    }
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
