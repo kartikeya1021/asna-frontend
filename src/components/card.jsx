fdd1e986b55c997f42ea0fd0961f5ad7816cb12e
Kartikeya <pandeykartikey012@gmail.com>
2023-08-23 23:18:31 +0530

    added clear button and done some changes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,18 +1,63 @@
-import React from 'react'
+import React from "react";
 
-export default function Card() {
+export default function Card({ card, type, editCard }) {
   return (
     <div
-            draggable
-            onDragStart={(e) => {
-              e.dataTransfer.setData("cardid", card.id);
-              e.dataTransfer.setData("source", "started");
-            }}
-            className="card"
-          >
-            {card.taskName}
-            <br />
-            {card.description}
-          </div>
-  )
+      draggable
+      onDragStart={(e) => {
+        e.dataTransfer.setData("cardid", card.id);
+        e.dataTransfer.setData("source", type);
+      }}
+      className="card"
+    >
+      {card.currentlyEditing ? (
+        <input
+          type="text"
+          value={card.taskName}
+          onChange={(e) =>
+            editCard(
+              card.id,
+              {
+                ...card,
+                taskName: e.target.value,
+              },
+              type
+            )
+          }
+        />
+      ) : (
+        card?.taskName
+      )}
+      <br />
+      {card.currentlyEditing ? (
+        <input
+          type="text"
+          value={card.description}
+          onChange={(e) =>
+            editCard(
+              card.id,
+              {
+                ...card,
+                description: e.target.value,
+              },
+              type
+            )
+          }
+        />
+      ):card?.description}
+      <i
+        onClick={() =>
+          editCard(
+            card.id,
+            {
+              ...card,
+              currentlyEditing: !card.currentlyEditing,
+            },
+            type
+          )
+        }
+        class="fa-solid fa-pen-to-square edit-icon"
+      ></i>
+    </div>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,25 @@
 import { useEffect, useState } from "preact/hooks";
 import React from "react";
 import "./home.css";
+import Card from "../../components/card";
 export default function Home() {
-  const [data, setData] = useState({
-    started: [
-      {
-        taskName: "Task 1",
-        status: "started",
-        date: new Date(),
-        id: 1,
-        description: "Hello world",
-      },
-    ],
-    inProgress: [],
-    done: [],
-  });
+  const [data, setData] = useState(
+    localStorage.getItem("userData")
+      ? JSON.parse(localStorage.getItem("userData"))
+      : {
+          started: [
+          ],
+          inProgress: [],
+          done: [],
+        }
+  );
 
   const [taskDetail, setTaskDetail] = useState({
     taskName: "Untitled",
     status: "",
     date: new Date(),
     description: "Add a description",
+    currentlyEditing: false,
   });
   const addTask = (type) => {
     if (type == "started") {
@@ -56,7 +55,7 @@ export default function Home() {
     const sourceStatus = e.dataTransfer.getData("source");
     var dataCopy = { ...data }; //deep copy
     var foundedCardIndex = dataCopy[sourceStatus].findIndex(
-      (card) => card.id == cardId
+      (card) => card?.id == cardId
     );
     var newCard = dataCopy[sourceStatus][foundedCardIndex];
     delete dataCopy[sourceStatus][foundedCardIndex];
@@ -64,84 +63,75 @@ export default function Home() {
     setData(dataCopy);
   };
 
-  return (
-    <div className="wrapper">
-      <div
-        className="started container"
-        onDrop={moveCard}
-        onDragOver={(e) => {
-          e.preventDefault();
-        }}
-      >
-        {data.started.map((card) => (
-          <div
-            draggable
-            onDragStart={(e) => {
-              e.dataTransfer.setData("cardid", card.id);
-              e.dataTransfer.setData("source", "started");
-            }}
-            className="card"
-          >
-            {card.taskName}
-            <br />
-            {card.description}
-          </div>
-        ))}
-        <button onClick={() => addTask("started")} className="card button">
-          + Add Card{" "}
-        </button>
-      </div>
+  const editCard = (cardId, newCardData, type) => {
+    var dataCopy = { ...data };
+    var foundedCardIndex = dataCopy[type].findIndex(
+      (card) => card?.id == cardId
+    );
+    dataCopy[type][foundedCardIndex] = newCardData;
+    setData(dataCopy);
+  };
+  useEffect(() => {
+    localStorage.setItem("userData", JSON.stringify(data));
+  }, [data]);
 
-      <div
-        className="inProgress container"
-        onDrop={moveCard}
-        onDragOver={(e) => {
-          e.preventDefault();
+  return (
+    <div>
+         <button
+        onClick={() => {
+          localStorage.clear();
+          window.location.reload();
         }}
       >
-        {data.inProgress.map((card) => (
-          <div
-            draggable
-            onDragStart={(e) => {
-              e.dataTransfer.setData("cardid", card.id);
-              e.dataTransfer.setData("source", "inProgress");
-            }}
-            className="card"
-          >
-            {card.taskName}
-            <br />
-            {card.description}
-          </div>
-        ))}
-        <button onClick={() => addTask("inProgress")} className="card button">
-          + Add Card{" "}
-        </button>
-      </div>
-      <div
-        className="done container"
-        onDrop={moveCard}
-        onDragOver={(e) => {
-          e.preventDefault();
-        }}
-      >
-        {data.done.map((card) => (
-          <div
-            draggable
-            onDragStart={(e) => {
-              e.dataTransfer.setData("cardid", card.id);
-              e.dataTransfer.setData("source", "done");
-            }}
-            className="card"
-          >
-            {card.taskName}
-            <br />
-            {card.description}
-          </div>
-        ))}
-        <button onClick={() => addTask("done")} className="card button">
-          + Add Card{" "}
-        </button>
-      </div>
+        Clear
+      </button>
+      <div className="wrapper">
+     
+     <div
+       className="started container"
+       onDrop={moveCard}
+       onDragOver={(e) => {
+         e.preventDefault();
+       }}
+     >
+       {data.started.map((card) => (
+         <Card card={card} type={"started"} editCard={editCard} />
+       ))}
+       <button onClick={() => addTask("started")} className="card button">
+         + Add Card{" "}
+       </button>
+     </div>
+
+     <div
+       className="inProgress container"
+       onDrop={moveCard}
+       onDragOver={(e) => {
+         e.preventDefault();
+       }}
+     >
+       {data.inProgress.map((card) => (
+         <Card card={card} type={"inProgress"} editCard={editCard} />
+       ))}
+       <button onClick={() => addTask("inProgress")} className="card button">
+         + Add Card{" "}
+       </button>
+     </div>
+     <div
+       className="done container"
+       onDrop={moveCard}
+       onDragOver={(e) => {
+         e.preventDefault();
+       }}
+     >
+       {data.done.map((card) => (
+         <Card card={card} type={"done"} editCard={editCard} />
+       ))}
+       <button onClick={() => addTask("done")} className="card button">
+         + Add Card{" "}
+       </button>
+     </div>
+   </div>
     </div>
+   
   );
 }
